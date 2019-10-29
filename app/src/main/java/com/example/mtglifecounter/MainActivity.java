82b1d15b67a9c04f0c5ff398da42583835638481
mick1221 <44068849+mick1221@users.noreply.github.com>
2019-10-29 14:20:22 -0500

    Update MainActivity.java
    created and intalized object refrences

--- app/src/main/java/com/example/mtglifecounter/MainActivity.java
+++ app/src/main/java/com/example/mtglifecounter/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mtglifecounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextView lifeTotal;
+        Button plusFive;
+        Button minusFive;
+        Button plusOne;
+        Button minusOne;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //intalizing objects
+
+        lifeTotal = findViewById(R.id.lifeTotal);
+        plusFive = findViewById(R.id.plusFive);
+        minusFive = findViewById(R.id.minusFive);
+        plusOne = findViewById(R.id.plusOne);
+        minusOne = findViewById(R.id.minusOne);
     }
 }
