bd21f68145ac8903845bbfce687b3f0b1ad63b41
mick1221 <44068849+mick1221@users.noreply.github.com>
2019-10-29 14:44:02 -0500

    Update MainActivity.java
    created one event handler

--- app/src/main/java/com/example/mtglifecounter/MainActivity.java
+++ app/src/main/java/com/example/mtglifecounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mtglifecounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,20 +12,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        TextView lifeTotal;
+        final TextView lifeTotal;
         Button plusFive;
         Button minusFive;
         Button plusOne;
         Button minusOne;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //intalizing objects
+        //assigning objects to widgets
 
         lifeTotal = findViewById(R.id.lifeTotal);
         plusFive = findViewById(R.id.plusFive);
         minusFive = findViewById(R.id.minusFive);
         plusOne = findViewById(R.id.plusOne);
         minusOne = findViewById(R.id.minusOne);
+
+
+        plusFive.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String lifeString = lifeTotal.getText().toString();
+                int life=Integer.parseInt(lifeString.trim());
+                life+=5;
+                lifeTotal.setText(life);
+            }
+        });
     }
 }
